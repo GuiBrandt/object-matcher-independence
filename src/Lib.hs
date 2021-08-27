5e0582d5565dcc61c512d6ddb822c63ae24fb648
GuiBrandt <guilherme.brandt@ifood.com.br>
2021-08-27 16:33:06 -0300

    Remove broken test comments

--- src/Lib.hs
+++ src/Lib.hs
@@ -48,20 +48,12 @@ maximalIndependentSets ms overlapping =
       edges = curry $ not . (`elem` incompatible)
    in getMaximalCliques edges ms
 
--- >>> let p = Not $ Conjunction [Attribute "x" ["1"], Attribute "z" ["4", "5"]]
--- >>> let q = Conjunction [Attribute "y" ["2"], Not $ Attribute "x" ["1", "3"]]
--- >>> case intersection p q of [] -> Nothing; xs -> Just $ head xs
--- Just [x != "1",x != "3",y == "2",z == "4"]
-
 intersection :: [ObjectMatcher] -> ObjectMatcher -> ObjectMatcher -> [[AttributeAssignment]]
 intersection rules p q =
   let formula = toFormula $ mconcat rules <> p <> q
       solutions = SAT.solve_all formula
       assignments = fmap (uncurry truth) . Map.assocs <$> solutions
    in simplify <$> assignments
-
--- >>> simplify ["x" :==: "1", "x" :!=: "2", "x" :!=: "3", "y" :!=: "4", "y" :!=: "5"]
--- [x == "1",y != "4",y != "5"]
 
 simplify :: [AttributeAssignment] -> [AttributeAssignment]
 simplify = isolatePositive <=< groupByAttribute
@@ -72,10 +64,6 @@ simplify = isolatePositive <=< groupByAttribute
     groupByAttribute :: [AttributeAssignment] -> [[AttributeAssignment]]
     groupByAttribute = groupBy (\p q -> attributeName p == attributeName q)
 
--- >>> let p = Not $ Conjunction [Attribute "x" ["1"], Attribute "z" ["4", "5"]]
--- >>> toFormula p
--- All [Var z == "4" :->: Not (Var z == "5")] :&&: Not (All [Var x == "1",Some [Var z == "4",Var z == "5"]])
-
 toFormula :: ObjectMatcher -> SAT.Formula AttributeAssignment
 toFormula = uncurry (:&&:) . (fundeps &&& toFormula')
   where
@@ -85,10 +73,6 @@ toFormula = uncurry (:&&:) . (fundeps &&& toFormula')
     toFormula' (Conjunction [matcher]) = toFormula' matcher
     toFormula' (Conjunction matchers) = SAT.All $ toFormula' <$> matchers
     toFormula' (Not matcher) = SAT.Not $ toFormula' matcher
-
--- >>> let p = Conjunction [Attribute "x" ["1", "2"], Attribute "y" ["3", "4"]]
--- >>> fundeps p
--- All [Var x == "1" :->: Not (Var x == "2"),Var y == "3" :->: Not (Var y == "4")]
 
 fundeps :: ObjectMatcher -> SAT.Formula AttributeAssignment
 fundeps = SAT.All . concatMap (uncurry fundeps' . second Set.elems) . Map.assocs . mergeAttributes
